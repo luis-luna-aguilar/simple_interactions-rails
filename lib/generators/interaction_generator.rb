45f14500b9de46711f3b3f6c90eff560b441b48c
Jose Boza <jaboza@gmail.com>
2015-06-17 11:13:42 -0600

    change argument name

--- lib/generators/interaction_generator.rb
+++ lib/generators/interaction_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators/named_base'
 
 class InteractionGenerator < ::Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
-  argument :params, default: []
+  argument :requirments, default: []
 
   def create_interaction_file
     template "interaction.rb", File.join('app/interactions/', class_path, "#{file_name}.rb")
--- lib/generators/templates/interaction.rb
+++ lib/generators/templates/interaction.rb
@@ -1,8 +1,8 @@
 <% module_namespacing do -%>
 class <%= class_name %>
   include SimpleInteraction
-  <% if params.any? %>
-  requires <%= params.map(&:to_sym).join(', ') %>
+  <% if requirments.any? %>
+  requires <%= requirments.map(&:to_sym).join(', ') %>
   <% end -%>
 end
 <% end -%>
