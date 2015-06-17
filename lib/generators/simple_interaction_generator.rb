f85965a080fd849b0dc18d4d653bcffd33e6d10c
Jose Boza <jaboza@gmail.com>
2015-06-17 10:15:13 -0600

    fix folders

---
+++ lib/generators/simple_interaction_generator.rb
@@ -0,0 +1,13 @@
+require 'rails/generators/base'
+
+class SimpleInteractionGenerator < ::Rails::Generators::Base
+  source_root File.expand_path("../templates", __FILE__)
+  argument :params, default: []
+
+  def create_initializer_file
+    template "interaction.rb", File.join('app/models', class_path, "#{file_name}.rb")
+  end
+
+  hook_for :test_framework
+
+end
