6b70f667b51e556619b5b38358a9dc50bcca4eba
Jose Boza <jaboza@gmail.com>
2015-06-17 14:26:12 -0600

    unit test generator

--- lib/generators/test_unit/interaction_generator.rb
+++ lib/generators/test_unit/interaction_generator.rb
@@ -5,7 +5,7 @@ class TestUnit::InteractionGenerator < ::Rails::Generators::NamedBase
   class_option :requirements, type: 'array', default: []
 
   def create_interaction_file
-    template "interaction.rb", File.join('test/interactions/', class_path, "#{file_name}.rb")
+    template "interaction_test.rb", File.join('test/interactions/', class_path, "#{file_name}_test.rb")
   end
 
 end
