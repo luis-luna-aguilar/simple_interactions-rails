330654514e88d0c912461dd9fd0b29f89628dcef
Jose Boza <jaboza@gmail.com>
2015-06-17 10:09:58 -0600

    remove class method

--- lib/simple_interaction/generators/simple_interaction_generator.rb
+++ lib/simple_interaction/generators/simple_interaction_generator.rb
@@ -7,8 +7,6 @@ module SimpleInteraction
     source_root File.expand_path("../templates", __FILE__)
     argument :params, default: []
 
-    check_class_collision
-
     def create_initializer_file
       template "interaction.rb", File.join('app/models', class_path, "#{file_name}.rb")
     end
